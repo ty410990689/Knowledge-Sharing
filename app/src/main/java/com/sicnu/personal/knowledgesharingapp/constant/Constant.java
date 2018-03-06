41bd997fef3c7b6fc6f0dc2baf8286a204ae553d
ty410990689 <2014432974@qq.com>
2018-03-06 10:48:02 +0800

    <Feat>:添加一些经常使用的常量

---
+++ app/src/main/java/com/sicnu/personal/knowledgesharingapp/constant/Constant.java
@@ -0,0 +1,15 @@
+package com.sicnu.personal.knowledgesharingapp.constant;
+
+/**
+ * Created by Administrator on 2018/3/5 0005.
+ */
+
+public class Constant {
+    public static final String CUSTOM_SERVER_BASE_URL_GUIDE="http://192.168.1.1/";
+    public static final String BASE_URL_GANK = "www.gank.io";
+
+    public static final String NET_STATES="netStatus";
+    public static final String WIFI_IS_CONNECT="wifiConnect";
+    public static final String NET_IS_CONNECT="netConnect";
+    public static final String NET_IS_DISCONNECT="netDisConnect";
+}
