4d3f4752122952df8fa451a1fa9e03c1f0ef85f4
ty410990689 <2014432974@qq.com>
2018-04-04 14:38:58 +0800

    <Feat>:调整美图模块布局，添加返回按钮
    <Body>:1.调整美图模块布局，添加返回按钮
           2.HomeActivity侧滑栏，添加跳转美图模块入口

--- app/src/main/java/com/sicnu/personal/knowledgesharingapp/pretty/activity/PrettyPictureActivity.java
+++ app/src/main/java/com/sicnu/personal/knowledgesharingapp/pretty/activity/PrettyPictureActivity.java
@@ -1,38 +1,41 @@
 package com.sicnu.personal.knowledgesharingapp.pretty.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.sicnu.personal.knowledgesharingapp.R;
 import com.sicnu.personal.knowledgesharingapp.pretty.adapter.PrettyRcAdapter;
 import com.sicnu.personal.knowledgesharingapp.pretty.contact.PrettyPictureContact;
 import com.sicnu.personal.knowledgesharingapp.pretty.model.databean.PrettyDataBean;
-import com.sicnu.personal.knowledgesharingapp.pretty.model.datasource.PrettyDataSource;
-import com.sicnu.personal.knowledgesharingapp.pretty.model.datasource.PrettyResponse;
 import com.sicnu.personal.knowledgesharingapp.pretty.presenter.PrettyPicturePresenter;
-import com.sicnu.personal.knowledgesharingapp.utils.YLog;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Administrator on 2018/4/3 0003.
  */
 
-public class PrettyPictureActivity extends Activity implements PrettyPictureContact.PrettyView {
+public class PrettyPictureActivity extends AppCompatActivity implements PrettyPictureContact.PrettyView {
     @BindView(R.id.toolbar_pretty)
     Toolbar toolbarPretty;
     @BindView(R.id.rc_pretty_picture)
     RecyclerView rcPrettyPicture;
+    @BindView(R.id.iv_pretty_back)
+    ImageView ivPrettyBack;
     private List<PrettyDataBean.ResultsBean> data;
     private PrettyPicturePresenter prettyPicturePresenter;
     private PrettyRcAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,12 +45,22 @@ public class PrettyPictureActivity extends Activity implements PrettyPictureCont
     }
 
     private void init() {
+        // setSupportActionBar(toolbarPretty);
         data = new ArrayList<>();
         prettyPicturePresenter = new PrettyPicturePresenter(this);
-        adapter = new PrettyRcAdapter(this,data);
-        rcPrettyPicture.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        adapter = new PrettyRcAdapter(this, data);
+        rcPrettyPicture.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         rcPrettyPicture.setAdapter(adapter);
         prettyPicturePresenter.firstRequstData(1);
+    }
+
+    @OnClick(R.id.iv_pretty_back)
+    public void setOnClickListener(View view){
+        switch (view.getId()){
+            case R.id.iv_pretty_back:
+                this.finish();
+                break;
+        }
     }
 
     @Override
